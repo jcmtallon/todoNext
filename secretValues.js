32cfb342cd8e9ec48b93f54e6f56d22340ab7469
Juanca MT <42070238+trueStoryJapan@users.noreply.github.com>
2019-12-21 16:59:11 +0900

    Delete secretValues.js

--- secretValues.js
+++
@@ -1,5 +0,0 @@
-// string used to connect to the app database.
-module.exports = {
-  dbConnection: 'mongodb+srv://tallyTrueStory:pro095678seda@cluster0-gyxb9.gcp.mongodb.net/test?retryWrites=true&w=majority',
-  sessionSecret: '5j4k3k3j4j4jljklj3io43jh3'
-};
